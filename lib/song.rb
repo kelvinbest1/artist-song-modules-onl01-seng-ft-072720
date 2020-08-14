e45a9ffb33f74b05a12569851dbf7bdc6d82f326
kelvin lanier <kelvinlanier@yahoo.com>
2020-08-14 14:53:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,2 +1,19 @@
 
 
+
+class Song
+
+  # extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include PARAMABLE
+  attr_accessor :name, :artist
+
+  @@songs = []
+
+  def self.all
+    @@songs
+  end
+  
+end
